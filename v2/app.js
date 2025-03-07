f8ae38c099634ac4fef8e9cdbbd0d6fc2d1c3fab
gelalias <jorgen.johannessen@mysafety.se>
2025-03-07 11:05:55 +0100

    Add files via upload

---
+++ v2/app.js
@@ -0,0 +1,33 @@
+// Declare the toggle variable before calling changeBackground
+let toggle = true; // Variable to toggle between two colors
+
+// Select The Button Object
+let button = document.querySelector("#button");
+console.log(button);
+changeBackground();
+
+// Add the event listener method to it, on click would call a function that changes background
+button.addEventListener("click", changeBackground);
+
+function changeBackground() {
+    console.log("Button clicked");
+    // Define two colors
+    let color1 = { r: 27, g: 174, b: 44 }; // Green
+    let color2 = { r: 200, g: 0, b: 0 }; // Red
+
+    // Choose color based on toggle
+    let color = toggle ? color1 : color2;
+    toggle = !toggle; // Toggle the value
+
+    // Change background color
+    document.body.style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+
+    // Change page's text color white or black
+    document.body.style.color = textColor(color.r, color.g, color.b);
+    button.style.color = textColor(color.r, color.g, color.b);
+}
+
+function textColor(r, g, b) {
+    // luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b
+    return 0.2126 * r + 0.7152 * g + 0.0722 * b > 128 ? 'black' : 'white';
+}
